d864b85909be6942051a8b03720efb38a5be093a
Goinza <gonzalo-am@hotmail.com>
2022-09-12 16:21:00 -0300

    Update colacp.c

--- colacp.c
+++ colacp.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct cola_con_prioridad {
+    unsigned int cantidad_elementos;
+    TNodo raiz;
+} * TColaCP;
+typedef struct nodo {
+    TEntrada entrada;
+    struct nodo * padre;
+    struct nodo * hijo_izquierdo;
+    struct nodo * hijo_derecho;
+} * TNodo;
+typedef struct entrada {
+    TClave clave;
+    TValor valor;
+} * TEntrada;
+
+typedef void * TClave;
+typedef void * TValor;
