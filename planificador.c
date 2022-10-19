7d02d1a6a3445f01081fb0b3e136e7638b9d2d58
Ever <everbarrios2001@gmail.com>
2022-10-19 16:05:06 -0300

    Funcion comparador y uso de la estructura cola con prioridad

--- planificador.c
+++ planificador.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "colacp.h"
 
 typedef struct ciudad {
     char * nombre;
@@ -23,12 +24,36 @@ int string_a_entero(char * string) {
     return resultado;
 }
 
+int comparador(TEntrada e1, TEntrada e2){
+    int clave1 = *((int *)e1->clave);
+    int clave2 = *((int *)e2->clave);
+    if (clave1 > clave2)
+        return -1;
+    if (clave1 < clave2)
+        return 1;
+    return 0;
+}
+
+int comparador_invertido(TEntrada e1, TEntrada e2){
+    return comparador(e2,e1);
+}
+
+float distancia_de_manhattan(int x, int y, TCiudad c){
+    float distancia = ((c->pos_x) - x) + (c->pos_y - y);
+    if (distancia < 0)
+        distancia = distancia * (-1);
+    return distancia;
+}
+
 int main(int argc, char *argv[]) {
     //Declaro variables
     ciudades = (TCiudad *) malloc(sizeof(TCiudad) * 4);
     const char limiter[2] = ";";
     int x, y;
     char * str = (char *) malloc(sizeof(char)*20);
+
+    //Decalor variable para la cola
+    TColaCP cola;
 
     //Abro archivo para lectura
     FILE * file;
@@ -66,20 +91,48 @@ int main(int argc, char *argv[]) {
     scanf("%s", str);
     x = string_a_entero(str);
     switch(x) {
-        case 1:
+        case 1:{
             printf("Ascendente");
+            cola = crear_cola_cp(comparador);
+        }
             break;
-        case 2:
+        case 2:{
             printf("Descendente");
+            cola = crear_cola_cp(comparador_invertido);
+        }
             break;
         case 3:
             printf("Reducir");
+            cola = crear_cola_cp(comparador);
             break;
         case 4:
             printf("Salir");
             break;
         default:
             printf("Error");
+    }
+
+    //Insertar en la cola
+    for (int i = 0; i < 4 ; i++){
+        float d;
+        TEntrada entrada;
+        entrada->clave = &d;
+        entrada->valor = *(ciudades + i);
+        if (x == 3){
+            if (cola->raiz != NULL){
+                float x = ((TCiudad)cola->raiz->entrada->valor)->pos_x;
+                float y = ((TCiudad)cola->raiz->entrada->valor)->pos_y;
+            }
+        }
+        d = distancia_de_manhattan(x,y, *(ciudades + i));
+        if (cp_insertar(cola,entrada) == FALSE)
+            printf("Ocurrio un error al insertar");
+    }
+    //Mostramos por pantalla el contenido de la cola
+    for (int i = 0; i < cola->cantidad_elementos; i ++){
+        TEntrada entr = cp_eliminar(cola);
+        TCiudad ciudad = entr->valor;
+        printf("%d %s",i,ciudad->nombre);
     }
 
     return 0;
